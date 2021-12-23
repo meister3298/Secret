71ee3cf06b5d87ac6bfbe93bf59baa7043e7ee02
Chong Wooi <chongwooi@gmail.com>
2021-12-23 16:28:00 +0800

    Level 4 authen with bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-cconst md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -36,20 +37,23 @@ app.get("/register", function(req, res){
 
 
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
 
-    newUser.save(function(err){
-        if(err)
-        {
-            console.log(err);
-        }
-        else
-        {
-            res.render("secrets");
-        }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function(err2){
+            if(err2)
+            {
+                console.log(err2);
+            }
+            else
+            {
+                res.render("secrets");
+            }
+        });
     });
 });
 
@@ -65,10 +69,12 @@ app.post("/login", function(req, res){
         else
         {
             if(foundUser){
-                if(foundUser.password === password)
-                {
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser, function(errBcrypt, result){
+                    if(result === true)
+                    {
+                        res.render("secrets");
+                    }
+                });
             }
         }
     });
